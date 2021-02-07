c6810aa152f8ae7dece0d41904d36ebe20c1dc8e
Lee Robertson <leechanr21@gmail.com>
2021-02-07 01:28:21 -0600

    Added base RSVP

--- src/pages/rsvp.js
+++ src/pages/rsvp.js
@@ -1,9 +1,19 @@
 import React from "react";
+import Layout from "../components/layout";
+import SEO from "../components/seo";
+
+import Banner from "../components/Banner";
 
 const RSVP = _ => {
-		return (
-				<div>rsvp</div>
-		)
+	return (
+		<Layout pageName="RSVP">
+			<SEO title="RSVP"/>
+			<div className="bannerContainer">
+				<Banner bannerSrc="bannerRSVP" />
+			</div>
+
+		</Layout>
+	)
 };
 
 export default RSVP;
